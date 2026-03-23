0832f08082f6aa6fbd3e01e5aa85a4ea5d44ef60
alucio06 <alucio2106@gmail.com>
2026-03-22 21:05:12 -0300

    adicionando validacao e simulando forms de contato

---
+++ Portfolio/script.js
@@ -0,0 +1,25 @@
+document.getElementById("form-contato").addEventListener("submit", function(event) {
+
+    event.preventDefault(); // impede envio real
+
+    let nome = document.getElementById("nome").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let mensagem = document.getElementById("mensagem").value.trim();
+
+    let retorno = document.getElementById("retorno-mensagem");
+
+    // validação simples
+    if(nome === "" || email === "" || mensagem === ""){
+        retorno.innerText = "Preencha todos os campos.";
+        retorno.style.color = "red";
+        return;
+    }
+
+    // simulação de envio
+    retorno.innerText = "Mensagem enviada com sucesso! Entrarei em contato em breve.";
+    retorno.style.color = "green";
+
+    // limpa formulário
+    document.getElementById("formContato").reset();
+
+});
